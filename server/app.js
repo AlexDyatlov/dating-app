929d51454e095965ca0fb749d71e686af0a81577
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-12 12:43:00 +0300

    Server setup, connected MongoDB, added data: professions, qualities

--- server/app.js
+++ server/app.js
@@ -2,11 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const chalk = require('chalk');
+const dotenv = require("dotenv");
 
+const initDatabase = require('./startUp/initDatabase');
+dotenv.config({ path: "./config.env" });
 const app = express();
 
-const PORT = config.get('port') ?? 8080;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, () =>
-  console.log(chalk.green('Server has been started on port'))
-);
+const PORT = config.get('port') ?? 8080;
+const urlMongo = process.env.MONGO_URI;
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('Production');
+// } else {
+//     console.log('Development');
+// }
+
+async function start() {
+  try {
+    mongoose.connection.once('open', () => {
+      initDatabase()
+    })
+    await mongoose.connect(urlMongo, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log(chalk.green('MongoDB connected'))
+
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on port ${PORT}...`))
+    );
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+
+start();
