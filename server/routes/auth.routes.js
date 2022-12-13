d5b75cf4062349e56cd372f6ba1929b8670fc2f7
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-13 09:54:15 +0300

    Creating logic for sign-up

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,16 +1,46 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const { generateUserData } = require('../utils/helpers');
+const tokenService = require('../services/token.service');
 const router = express.Router({ mergeParams: true });
 
 router.post('/signUp', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const exitingUser = await User.findOne({ email });
+    if (exitingUser) {
+      return res.status(400).json({
+        error: {
+          message: 'EMAIL_EXISTS',
+          code: 400
+        }
+      });
+    }
 
-})
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = User.create({
+      ...generateUserData(),
+      ...req.body,
+      password: hashedPassword
+    });
+
+    const tokens = tokenService.generate({ _id: newUser._id });
+    res.status(201).send({ ...tokens, userId: newUser._id });
+
+  } catch (e) {
+    res.status(500).json({
+      message: 'На сервере произошла ошибка. Попробуйте позже.'
+    });
+  }
+});
 
 router.post('/signInWithPassword', async (req, res) => {
-    
+
 })
 
 router.post('/token', async (req, res) => {
-    
+
 })
 
 module.exports = router;
