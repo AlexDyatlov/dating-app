9979184cfbf358ca36688a6bd6be45aac6f29906
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-15 16:42:46 +0300

    Typo, importing the auth module

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../middleware/auth.middleware');
 const router = express.Router({ mergeParams: true });
 const Comment = require('../models/Comment');
 
