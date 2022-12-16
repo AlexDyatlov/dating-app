5ff4135aeb9188de4bfe6b220c1c8af3c16553fd
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-16 11:58:58 +0300

    Fixed errors during registration

--- client/src/app/store/users.js
+++ client/src/app/store/users.js
@@ -4,7 +4,6 @@ import authService from '../services/authService';
 import localStorageService from '../services/localStorageService';
 import userService from '../services/userService';
 
-import getRandomInt from '../utils/getRandomInt';
 import history from '../utils/history';
 import { generateAuthError } from '../utils/generateAuthError';
 
@@ -79,14 +78,11 @@ const {
   usersRequestFailed,
   authRequestSuccess,
   authRequestFailed,
-  userCreated,
   userLoggedOut,
   userUpdateSuccessed
 } = actions;
 
 const authRequested = createAction('users/authRequested');
-const userCreateRequested = createAction('users/userCreateRequested');
-const createUserFailed = createAction('users/createUserFailed');
 const userUpdateRequested = createAction('users/userUpdateRequested');
 const userUpdateFailed = createAction('users/userUpdateFailed');
 
@@ -112,27 +108,14 @@ export const login =
     };
 
 export const signUp =
-  ({ email, password, ...rest }) =>
+  (payload) =>
     async (dispatch) => {
       dispatch(authRequested());
       try {
-        const data = await authService.register({ email, password });
+        const data = await authService.register(payload);
         localStorageService.setTokens(data);
-        dispatch(authRequestSuccess({ userId: data.localId }));
-        dispatch(
-          createUser({
-            _id: data.localId,
-            email,
-            rate: getRandomInt(1, 5),
-            completedMeetings: getRandomInt(0, 200),
-            image: `https://avatars.dicebear.com/api/avataaars/${(
-              Math.random() + 1
-            )
-              .toString(36)
-              .substring(7)}.svg`,
-            ...rest
-          })
-        );
+        dispatch(authRequestSuccess({ userId: data.userId }));
+        history.push('/users');
       } catch (error) {
         dispatch(authRequestFailed(error.message));
       }
@@ -143,19 +126,6 @@ export const logOut = () => (dispatch) => {
   dispatch(userLoggedOut());
   history.push('/');
 };
-
-function createUser(payload) {
-  return async function (dispatch) {
-    dispatch(userCreateRequested());
-    try {
-      const { content } = await userService.create(payload);
-      dispatch(userCreated(content));
-      history.push('/users');
-    } catch (error) {
-      dispatch(createUserFailed(error.message));
-    }
-  };
-}
 
 export const updateUser = (payload) => async (dispatch) => {
   dispatch(userUpdateRequested());
