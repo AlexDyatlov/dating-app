7eb8c8bbc10f36c8a9f81bc62539bd4b45f543ec
AlexDyatlov <aleks78.gooogle@gmail.com>
2022-12-16 09:52:05 +0300

    Integration start. Refactoring

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const chalk = require('chalk');
+const cors = require('cors');
 const dotenv = require("dotenv");
 const initDatabase = require('./startUp/initDatabase');
 const routes = require('./routes');
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use('/api', routes);
 
 const PORT = config.get('port') ?? 8080;
